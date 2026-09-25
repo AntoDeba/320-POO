753d75514f8bbee7bc586664ca1cbaa1f57cdd25
AntoDeba <antoni.debaz@gmail.com>
2026-09-25 11:27:45 +0200

    feat(tests) creation de fonction simple de test (non-fonctionnelle)

---
+++ dossierPersonnel/TheorieTest/TestsOfTests/ActualTests/Test1.cs
@@ -0,0 +1,22 @@
+﻿using TestsOfTests;
+namespace ActualTests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //arrange
+            int x = 10;
+            int y = 10;
+
+            // Act
+            int res = MyMath.Somme(x, y);
+
+
+            //Assert
+            Assert.AreEqual(20, res);
+        }
+    }
+}
---
+++ dossierPersonnel/TheorieTest/TestsOfTests/TestsOfTests/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsOfTests
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+    }
+
+    public class MyMath
+    {
+        public static int Somme(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
